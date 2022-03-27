37e9291cbfa10d5afa406a14d9c287c9b182297b
Warzecha <warzecha@fev.com>
2022-03-27 16:07:50 +0200

    Add basic options

---
+++ lua/user/options.lua
@@ -0,0 +1,29 @@
+local options = {
+  encoding = 'utf-8',
+	expandtab = true,
+	showtabline = 1,
+	tabstop = 2,
+	shiftwidth = 2,
+  smartindent = true,
+  smartcase = true,
+
+  number = true,
+  relativenumber  = true,
+
+  wrap = true,
+
+  hlsearch = true,
+  incsearch = true,
+  ignorecase = true,
+
+  conceallevel = 0,
+  completeopt = { 'menuone', 'preview' },  
+
+  scrolloff = 8,
+  mouse = 'a',
+  guifont = 'Fira/ Code:h20',
+}
+
+for key, value in pairs(options) do
+	vim.opt[key] = value
+end
