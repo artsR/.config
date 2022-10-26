5cc5e4a6e37adfe57e525317f083a3eb3df0a483
artsR <10280256+artsR@users.noreply.github.com>
2022-10-26 13:53:23 +0200

    Delete lua/artur directory

--- lua/artur/hop/init.lua
+++
@@ -1,3 +0,0 @@
--- Use better keys for the bépo keyboard layout and set
--- a balanced distribution of terminal / sequence keys
-require'hop'.setup { keys='etovxqpdygfblzhckisuran', jump_on_sole_occurrence=true }
--- lua/artur/lsp/settings/solidity-ls.lua
+++
@@ -1,8 +0,0 @@
-return {
-  -- on_attach = on_attach, -- probably you will need this.
-  -- capabilities = capabilities,
-  settings = {
-    -- example of global remapping
-    solidity = { includePath = 'C:/Users/warzecha/AppData/Local/nvim-data/lsp_servers/solc/', remapping = { ["@OpenZeppelin/"] = 'OpenZeppelin/openzeppelin-contracts@4.6.0/' } }
-  },
-}
